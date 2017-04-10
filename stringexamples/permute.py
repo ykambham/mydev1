857d8236a68a36404c015a6892b34a0f2f00e989
Yugesh Kambham <yugesh.red.kambham@hpe.com>
2017-04-10 08:27:12 -0700

    Modified permute file

--- stringexamples/permute.py
+++ stringexamples/permute.py
@@ -2,26 +2,27 @@
 # with itertools do below steps
 # import itertools
 #[a for a in itertools.permutations(numbers)]
-def permute(LIST):
-    length=len(LIST)
+def permute(a_list):
+    length=len(a_list)
     if length <= 1:
-        yield LIST
+        yield a_list
     else:
         for n in range(0,length):
-            for end in permute(LIST[:n] + LIST[n+1:]):
-                yield [LIST[n]] + end
+            for end in permute(a_list[:n] + a_list[n+1:]):
+                yield [a_list[n]] + end
+
+print [item for item in permute([1, 2, 3])]
 
 # Python library way to get all_combinations with repeats
-def all_combinations(*args, **kwds):
-    pools = map(tuple, args) * kwds.get('repeat', 1)
+def all_combinations(a_list, repeat):
+    pools = [a_list] * repeat
     result = [[]]
     for pool in pools:
         result = [x+[y] for x in result for y in pool]
     for prod in result:
-        yield tuple(prod)
-        
-for item in  all_combinations([1, 2, 3], repeat=3):
-    print item
+        yield prod
+ 
+print [item for item in all_combinations([1, 2, 3], 3)]
 
 '''
 Output of result in each iteration:
